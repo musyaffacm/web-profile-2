15ed713e7d3c6e9bf5223f30eb9cad5a04f340f2
musyaffacm <musyaffa220@gmail.com>
2024-10-31 07:11:35 +0700

    feat: install poppins and passion one fonts

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,20 @@
-import Navbar from "@/components/Navbar";
+import { Poppins, Passion_One } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-poppins'
+})
+
+const passion_one = Passion_One({
+  subsets: ['latin'],
+  variable: '--font-passion-one',
+  weight: ['400', '700', '900']
+})
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <main className={`${poppins.className} ${poppins.variable} ${passion_one.variable}`}>
+    </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily:{
+        passion_one: ['var(--font-passion-one)'],
+        poppins: ['var(--font-poppins)']
+      }
     },
   },
   plugins: [],
